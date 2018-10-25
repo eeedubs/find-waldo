6e2a3e199e592685daec16559247e6418c948d0b
Eric Wilson <ericswilson94@gmail.com>
2018-10-25 18:42:16 +0000

    updated find-waldo

--- find-waldo.js
+++ find-waldo.js
@@ -1,10 +1,11 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
+  arr.forEach(function(element, i) { // goes through each element
+    // each iteration is temporarily represented by i
+    if (element === "Waldo") {
       found(i);   // execute callback
-    }
-  }
+    };
+  });
 }
 
 function actionWhenFound(index) {
